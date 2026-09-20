8955b6e98fa2a46b1a83c0ececee47624b9b03af
ttaannsseell73-alt <ttaannsseell73@gmail.com>
2026-09-20 18:31:58 +0300

    feat: export generic signal engine contract

--- src/freqtrade_research_lab/__init__.py
+++ src/freqtrade_research_lab/__init__.py
@@ -6,16 +6,20 @@ from .dataset import MarketFile, discover_market_files, load_ohlcv
 from .event_study import (
     EventStudyConfig,
     analyze_pair,
+    analyze_signals,
     build_candidate_tables,
     build_discovery_table,
 )
 from .experiment import ExperimentSpec, tag_result_frame
+from .signals import SignalSet, macd_crossover_signals, validate_signal_set
 
 __all__ = [
     "EventStudyConfig",
     "ExperimentSpec",
     "MarketFile",
+    "SignalSet",
     "analyze_pair",
+    "analyze_signals",
     "apply_global_fdr",
     "build_candidate_tables",
     "build_discovery_table",
@@ -23,7 +27,9 @@ __all__ = [
     "combine_discovery_frames",
     "discover_market_files",
     "load_ohlcv",
+    "macd_crossover_signals",
     "tag_result_frame",
+    "validate_signal_set",
     "write_catalog_bundle",
 ]
 
