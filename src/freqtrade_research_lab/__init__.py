9cbd999fb9e39c7a4637914b8a46e9e42bd0c597
ttaannsseell73-alt <ttaannsseell73@gmail.com>
2026-09-20 18:38:48 +0300

    feat: export data provenance helpers

--- src/freqtrade_research_lab/__init__.py
+++ src/freqtrade_research_lab/__init__.py
@@ -2,7 +2,15 @@
 
 from .catalog_io import write_catalog_bundle
 from .comparison import apply_global_fdr, build_system_coverage, combine_discovery_frames
-from .dataset import MarketFile, discover_market_files, load_ohlcv
+from .dataset import (
+    CatalogSelection,
+    MarketFile,
+    discover_market_files,
+    fingerprint_market_files,
+    load_catalog_selection,
+    load_ohlcv,
+    sha256_file,
+)
 from .event_study import (
     EventStudyConfig,
     analyze_pair,
@@ -15,6 +23,7 @@ from .run_io import prepare_fresh_output_dir
 from .signals import SignalSet, macd_crossover_signals, validate_signal_set
 
 __all__ = [
+    "CatalogSelection",
     "EventStudyConfig",
     "ExperimentSpec",
     "MarketFile",
@@ -27,9 +36,12 @@ __all__ = [
     "build_system_coverage",
     "combine_discovery_frames",
     "discover_market_files",
+    "fingerprint_market_files",
+    "load_catalog_selection",
     "load_ohlcv",
     "macd_crossover_signals",
     "prepare_fresh_output_dir",
+    "sha256_file",
     "tag_result_frame",
     "validate_signal_set",
     "write_catalog_bundle",
