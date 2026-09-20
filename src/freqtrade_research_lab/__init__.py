8b5daa5935091a0f165b2891becc56eb31bc4c82
ttaannsseell73-alt <ttaannsseell73@gmail.com>
2026-09-20 18:09:00 +0300

    feat: export experiment identity helpers

--- src/freqtrade_research_lab/__init__.py
+++ src/freqtrade_research_lab/__init__.py
@@ -2,13 +2,16 @@
 
 from .dataset import MarketFile, discover_market_files, load_ohlcv
 from .event_study import EventStudyConfig, analyze_pair, build_candidate_tables
+from .experiment import ExperimentSpec, tag_result_frame
 
 __all__ = [
     "EventStudyConfig",
+    "ExperimentSpec",
     "MarketFile",
     "analyze_pair",
     "build_candidate_tables",
     "discover_market_files",
     "load_ohlcv",
+    "tag_result_frame",
 ]
 
